49e2161c1438d82149882d7bf03038cfa56cd0f6
Victor Bagu <victor@digitalpath.co.za>
2018-10-21 23:35:06 +0200

    Apply IndexedDB API

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -2,7 +2,6 @@
  * Common database helper functions.
  */
 class DBHelper {
-
   /**
    * Database URL.
    * Change this to restaurants.json file location on your server.
@@ -13,34 +12,81 @@ class DBHelper {
   }
 
   /**
+   * IndexDB API setup
+   */
+  static openMWSdb() {
+    if (!navigator.serviceWorker) {
+      return Promise.resolve();
+    }
+    return idb.open('MWSdb', 1, function(upgradeDb) {
+      const restaurantStore = upgradeDb.createObjectStore('restaurants', {
+        keyPath: 'name'
+      })
+    })
+  }
+
+  /**
+   * retrieve from IndexDB
+   */
+  static getRestaurantsFromDB(){
+    return DBHelper.openMWSdb().then((db) => {
+      if (!db) return;
+      const tx = db.transaction('restaurants');
+      const restaurantsStore = tx.objectStore('restaurants');
+      return restaurantsStore.getAll().then((restaurants) => {
+        return restaurants;
+      });
+    })
+  }
+
+  /**
+   * store on IndexDB
+   */
+  static storeRestaurants(restaurants){
+    DBHelper.openMWSdb().then((db) => {
+      if(!db) return;
+      const tx = db.transaction('restaurants','readwrite');
+      const restaurantsStore = tx.objectStore('restaurants');
+      const restaurantKeys = Object.keys(restaurants);
+      restaurantKeys.forEach((restaurantKey) => {
+        restaurantsStore.put(restaurants[restaurantKey])
+      })
+    })
+  }
+
+
+
+
+  /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
     let sourceURL;
     sourceURL = DBHelper.DATABASE_URL;
     fetch(sourceURL)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => callback(null, data))
-    .catch(error => callback(`Failed request. ${error}`, null));
-  
+      .then(response => {
+        return response.json();
+      })
+      .then(data => callback(null, data))
+      .catch(error => callback(`Failed request. ${error}`, null));
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-  // fetch all restaurants with proper error handling.
+    // fetch all restaurants with proper error handling.
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
+        if (restaurant) {
+          // Got the restaurant
           callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
+        } else {
+          // Restaurant does not exist in the database
+          callback("Restaurant does not exist", null);
         }
       }
     });
@@ -81,17 +127,23 @@ class DBHelper {
   /**
    * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
    */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+  static fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    callback
+  ) {
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         let results = restaurants;
-        if (cuisine != 'all') { // filter by cuisine
+        if (cuisine != "all") {
+          // filter by cuisine
           results = results.filter(r => r.cuisine_type == cuisine);
         }
-        if (neighborhood != 'all') { // filter by neighborhood
+        if (neighborhood != "all") {
+          // filter by neighborhood
           results = results.filter(r => r.neighborhood == neighborhood);
         }
         callback(null, results);
@@ -109,9 +161,13 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood);
+        const neighborhoods = restaurants.map(
+          (v, i) => restaurants[i].neighborhood
+        );
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i);
+        const uniqueNeighborhoods = neighborhoods.filter(
+          (v, i) => neighborhoods.indexOf(v) == i
+        );
         callback(null, uniqueNeighborhoods);
       }
     });
@@ -129,7 +185,9 @@ class DBHelper {
         // Get all cuisines from all restaurants
         const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i);
+        const uniqueCuisines = cuisines.filter(
+          (v, i) => cuisines.indexOf(v) == i
+        );
         callback(null, uniqueCuisines);
       }
     });
@@ -139,29 +197,32 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return `./restaurant.html?id=${restaurant.id}`;
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.id}.jpg`);
+    return `/img/${restaurant.id}.jpg`;
   }
 
   /**
    * Map marker for a restaurant.
    */
-   static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
-    const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
-      })
-      marker.addTo(newMap);
+  static mapMarkerForRestaurant(restaurant, map) {
+    // https://leafletjs.com/reference-1.3.0.html#marker
+    const marker = new L.marker(
+      [restaurant.latlng.lat, restaurant.latlng.lng],
+      {
+        title: restaurant.name,
+        alt: restaurant.name,
+        url: DBHelper.urlForRestaurant(restaurant)
+      }
+    );
+    marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -172,6 +233,4 @@ class DBHelper {
     );
     return marker;
   } */
-
 }
-
